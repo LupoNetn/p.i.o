1630fed27e7f500fa7e774d9f55204236deb6be6
oluwatobi <luponet07@gmail.com>
2025-11-04 23:23:39 +0100

    changed cors rules

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,10 +16,22 @@ const port = process.env.PORT
 
 
 //middlewares
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://piooo.vercel.app',
+];
+
 app.use(cors({
-  origin: "http://localhost:5173", 
-  credentials: true,             
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
 }));
+
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
